8cd74ee813f68473112c9fa7d807eb9c0a4868c9
Forward Costa Rica <ngonzalezfwd@gmail.com>
2026-06-08 13:38:29 -0600

    Implementacion del bashboard del degresado

--- Frontend/src/Routes/Routing.tsx
+++ Frontend/src/Routes/Routing.tsx
@@ -1,5 +1,8 @@
-import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "../Pages/Log/Login";
+import DashboardEngresado from "../Pages/Home/DashboardEngresado/DashboardEngresado";
+import PerfilEngresado from "../Pages/Profile/PerfilEngresado/PerfilEngresado";
+
 function Routing() {
     return (
         <Router>
@@ -7,6 +10,8 @@ function Routing() {
 
                 {/* Rutas publicas */}
                 <Route path="/" element={<Login />} />
+                <Route path="/DashboardEngresado" element={<DashboardEngresado />} />
+                <Route path="/PerfilEngresado" element={<PerfilEngresado />} />
 
                 {/* Rutas privadas */}
                 
